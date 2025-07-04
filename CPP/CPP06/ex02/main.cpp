0b0df736aa1de9c7d8de4e599a7a1d8247c7e513
yseguin <youvataque@icloud.com>
2025-07-04 10:18:09 +0200

    correction ex02 cpp06 (quand on lit à la va vite les consignes...)

--- CPP/CPP06/ex02/main.cpp
+++ CPP/CPP06/ex02/main.cpp
@@ -1,11 +1,59 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
 #include "Base.hpp"
+
+
+Base* generate(void) {
+	int random = rand() % 3;
+	if (random == 0)
+		return new A();
+	else if (random == 1)
+		return new B();
+	else
+		return new C();
+}
+
+void identify(Base* p) {
+	if (dynamic_cast<A*>(p))
+		std::cout << "A" << std::endl;
+	else if (dynamic_cast<B*>(p))
+		std::cout << "B" << std::endl;
+	else if (dynamic_cast<C*>(p))
+		std::cout << "C" << std::endl;
+}
+
+void identify(Base& p) {
+	try {
+		A& a = dynamic_cast<A&>(p);
+		(void)a;
+		std::cout << "A" << std::endl;
+	} catch (std::exception error) {
+		(void)error;
+		try {
+			B& b = dynamic_cast<B&>(p);
+			(void)b;
+			std::cout << "B" << std::endl;
+		} catch (std::exception error) {
+			(void)error;
+			try {
+				C& c = dynamic_cast<C&>(p);
+				(void)c;
+				std::cout << "C" << std::endl;
+			} catch (std::exception error) {
+				(void)error;
+			}
+		}
+	}
+}
 
 void printline(const std::string& title) {
 	std::cout << "\033[1;36m" << title << "\033[0m" << std::endl;
 }
+
 int main() {
 	printline("TEST DYNAMIC CAST IDENTIFICATION");
 
@@ -14,15 +62,15 @@ int main() {
 	for (int i = 0; i < 10; ++i) {
 		printline("Test #" + std::to_string(i + 1));
 
-		Base* random = Base::generate();
+		Base* random = generate();
 
 		std::cout << "[*] Pointer address : " << random << std::endl;
 
 		std::cout << "[>] identify(Base*)  : ";
-		Base::identify(random);
+		identify(random);
 
 		std::cout << "[>] identify(Base&)  : ";
-		Base::identify(*random);
+		identify(*random);
 
 		delete random;
 	}
