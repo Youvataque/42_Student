69cd9fb497bef6cd3fb6d7b6efbca5b3ab54ed53
yseguin <youvataque@icloud.com>
2025-07-07 13:21:30 +0200

    fix affichage

--- CPP/CPP07/ex01/main.cpp
+++ CPP/CPP07/ex01/main.cpp
@@ -2,7 +2,6 @@
 #include <iostream>
 #include <string>
 
-// --- Fonctions sur int ---
 void printInt(const int& n) {
 	std::cout << "Const print int: " << n << std::endl;
 }
@@ -15,7 +14,6 @@ void incrementInt(int& n) {
 	n++;
 }
 
-// --- Fonctions sur string ---
 void printString(const std::string& str) {
 	std::cout << "String: " << str << std::endl;
 }
@@ -25,29 +23,30 @@ void toUpper(std::string& str) {
 		str[i] = std::toupper(str[i]);
 }
 
-// --- Titre coloré ---
 void printTitle(const std::string& title) {
 	std::cout << "\033[1;36m" // gras cyan
 			  << "\n=== " << title << " ===\n"
 			  << "\033[0m";
 }
 
+
+
 int main() {
 	printTitle("INT ARRAY");
 	int intTab[] = {1, 2, 3, 4, 5};
 	iter(intTab, 5, printInt);
 	iter(intTab, 5, incrementInt);
-	iter(intTab, 5, printInt); // vérifier que les valeurs ont changé
+	iter(intTab, 5, printInt);
 
 	printTitle("STRING ARRAY");
 	std::string strTab[] = {"hello", "world", "42"};
 	iter(strTab, 3, printString);
 	iter(strTab, 3, toUpper);
-	iter(strTab, 3, printString); // vérifier passage en majuscules
+	iter(strTab, 3, printString);
 
 	printTitle("DOUBLE ARRAY (TEMPLATE FUNC)");
 	double dtab[] = {1.1, 2.2, 3.3};
-	iter(dtab, 3, printDouble); // print générique
+	iter(dtab, 3, printDouble);
 
 	return 0;
 }
